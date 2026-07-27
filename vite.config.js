883399276bcc20eb1980af2a395cf5a1f31531aa
Jacob Andrés Cheuquiel <jacob.cheuquiel@gmail.com>
2026-07-27 18:45:35 -0400

    adding js to vite file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/global-toast.js',
+                'resources/js/product-slider.js',
+                'resources/js/slider.js',
+                'resources/js/standard-product.js',
+                'resources/js/toast.js',
+                'resources/js/cart/cart.js',
+                'resources/js/cart/remove.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
