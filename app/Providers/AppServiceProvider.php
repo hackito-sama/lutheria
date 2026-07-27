4f4ef9fe05c4d299ab51d5565d0b248debc56679
Jacob Andrés Cheuquiel <jacob.cheuquiel@gmail.com>
2026-07-27 19:45:01 -0400

    seeders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,8 +18,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
+
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Orden recomendado: primero datos base como colores, luego productos y opciones, finalmente pickups u otros datos dependientes
+        $this->call([
+            ColorSeeder::class,
+            ProductStandardSeeder::class,
+            ProductOptionsSeeder::class,
+            PickupSeeder::class,
         ]);
     }
 }
